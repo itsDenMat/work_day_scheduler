f4ee6105c55457115107a170298f23acb193dbb7
itsDenMat <dennismateo93@gmail.com>
2022-07-19 23:18:38 -0700

    Add links in README

--- Develop/script.js
+++ Develop/script.js
@@ -15,3 +15,88 @@ let saveBtn = $('.saveBtn');
 let timeblock;
 let timerInterval;
 let timeblockID = $("textarea[id*='timeblock']");
+
+// Functions to render date and events to DOM and update background color
+function init() {
+    momentDate();
+    renderEvents();
+    BGColors();
+};
+
+// Generate current date to display in div below jumnbotron
+function momentDate() {
+    currentDate = moment().format('dddd, LL, h:mm a');
+    currentDateEl.text(currentDate);
+};
+
+// Renders event pulled from local storage to DOM
+function renderEvents() {
+    for (let i = 0; i < timeArr.length; i++) { 
+        $('[id^=timeblock-]').each(function (i, v) {
+            $(v).val(localStorage.getItem(timeArr[i]));
+        })
+    }
+};
+
+// Click handler for save buttons
+saveBtn.on('click', saveHandler);
+
+// Will pull time and date value when save button is clicked
+function saveHandler(event) {
+    event.preventDefault();
+
+    // Set value to time when save button is clicked
+    eventTime = $(this).attr('id').split('-')[1];
+    // Set value to input when save button is clicked
+    eventTxt = $(this).siblings('textarea[name^="timeblock"]').val().trim();
+    // Call function to store in local storage
+    saveEvents();
+};
+
+// Save time and input values to local storage
+function saveEvents() {
+    localStorage.setItem(eventTime, eventTxt);
+};
+
+// Function to change color and class (present, past, future) of blocks as time passes
+function BGColors() {
+  
+    timeblockID.each(function () {
+    timeBlock = $(this).attr('id').split('-')[1];
+    timeBlock = parseInt(moment(timeBlock, 'H').format('H'));
+    currentTime = parseInt(moment().format('H'));
+    
+    if (currentTime < timeBlock) {
+        $(this).removeClass('past present');
+        $(this).addClass('future');
+    } else if (currentTime === timeBlock) {
+        $(this).removeClass('past future');
+        $(this).addClass('present');
+    } else if (currentTime > timeBlock) {
+        $(this).removeClass('present future');
+        $(this).addClass('past');
+    } else {
+        console.log("Time Error");
+    }
+    })
+};
+
+// Changes date and time with colors every minute
+function intervalMinute() {
+    var currentDateSeconds = new Date().getSeconds();
+    if (currentDateSeconds == 0) {
+        setInterval(momentDate, 60000);
+        setInterval(BGColors, 60000);
+    } else {
+        setTimeout(function () {
+            intervalMinute();
+        }, (60 - currentDateSeconds) * 1000);
+    }
+    momentDate();
+    BGColors();
+};
+
+intervalMinute();
+
+// Executes app
+init();
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Work_Day_Scheduler
+# Work Day Scheduler
+
+This is a simple calendar application that allows a user to save events for each hour. 
+
+This exercise furthered experience dynamically updating HTML and CSS using jQuery and utilized Moment.js to exhibit day and time.
+
+>Note: Starter HTML and CSS codes were provided but was minimally edited for aesthetic purposes. 
+
+## Accessing the Application
+
+To access the application, you can follow the deployment link or download the source files in the GitHub repository linked below.
+
+<p><strong>Deployment Link:</strong> <a href="https://itsdenmat.github.io/work_day_scheduler/">Work Day Scheduler</a></p>
+<p><strong>GitHub:</strong> <a href="https://github.com/itsDenMat/work_day_scheduler">GitHub Work Day Scheduler Repository</a></p>
+
+## Application Functionality
+
+* Application with show the current day and time at the top of the calendar
+<img src="./assets/date-time.png">
+
+* App is presented with timeblocks for standard business hours.
+<img src="./assets/time-business-hour.png">
+
+* Each timeblock is indicated by color if it is in the past (gray), present (yellow), future (green).
+<img src="./assets/colored-block.png">
+
+* Each timeblock has text area and a save button to save the information.
+<img src="./assets/text-area-savebtn.png">
+
+* When the save button is clicked, each events will be saved in the local storage.
+<img src="./assets/local-storage.png">
+
+* Events will continuously show even if the page is refreshed.
+<img src="./assets/refresh-page.gif">
